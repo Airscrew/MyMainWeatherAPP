d01dfd1559936efadbd3d2bffa3b9eb03f690a76
Airscrew <sonyxperiaz1one@gmail.com>
2022-01-21 15:14:43 +0200

    update

--- app/src/main/java/com/example/mymainweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/mymainweatherapp/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.mymainweatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import org.jetbrains.anko.doAsync
 import java.net.URL
 import android.util.Log
+import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+
+
+import android.R
+
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     private var curr_city: TextView? = null
@@ -18,12 +42,14 @@ class MainActivity : AppCompatActivity() {
     private var atmo_press: TextView? = null
     private var humidity: TextView? = null
     private var wind_speed: TextView? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         curr_city = findViewById(R.id.curr_city)
         curr_temp = findViewById(R.id.curr_temp)
         min_temp = findViewById(R.id.min_temp)
@@ -31,6 +57,10 @@ class MainActivity : AppCompatActivity() {
         atmo_press = findViewById(R.id.atmo_press)
         humidity = findViewById(R.id.humidity)
         wind_speed = findViewById(R.id.wind_speed)
+
+        Task<Location> locationTask = fused
+
+
 
         var lat: Double = 33.44
         var lon: Double = -94.04
@@ -41,7 +71,9 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val apiResponse = URL(url).readText()
             Log.d("Message", apiResponse)
+
         }
+
 
 
 
